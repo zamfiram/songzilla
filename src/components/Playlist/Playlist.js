8fc5c7b211afcd4e4f48ce7250aeb5ff862a17b0
Madalina <madalina.zamfira@gmail.com>
2020-10-12 00:13:25 +0200

    add tracklist component

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './Playlist.css';
+import TrackList from "../TrackList/TrackList";
 
 const Playlist = () => {
   return (
     <div className="Playlist">
       <input defaultValue={'New Playlist'} />
+      <TrackList/>
       <button className="Playlist-save">Save this song to your Spotify playlist</button>
     </div>
   );
---
+++ src/components/TrackList/TrackList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TrackList.css';
+
+const TrackList = () => {
+    return (
+        <div className="TrackList">
+            <p>TrackList 1</p>
+            <p>TrackList 2</p>
+            <p>TrackList 3</p>
+        </div>
+    )
+}
+
+export default TrackList
