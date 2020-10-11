9fd621a223cf1a82cf9687e74608dc2b2aa2019a
Madalina <madalina.zamfira@gmail.com>
2020-10-11 23:33:33 +0200

    add background image & modify app.css and html

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Song<span class="highlight">zilla</span></h1>
+    <div class="App">
+      <div class="App-playlist">
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
