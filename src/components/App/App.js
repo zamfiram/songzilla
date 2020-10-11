33fe62ba98bbb99454f02258ba5687f75001b599
Madalina <madalina.zamfira@gmail.com>
2020-10-11 23:47:16 +0200

    add search bar component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SearchBar from "../SearchBar/SearchBar";
 
 function App() {
   return (
     <div>
-    <h1>Song<span class="highlight">zilla</span></h1>
-    <div class="App">
-      <div class="App-playlist">
+      <h1>
+        Song<span className="highlight">zilla</span>
+      </h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist"></div>
       </div>
     </div>
-  </div>
   );
 }
 
