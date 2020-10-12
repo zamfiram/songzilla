b988b2dc333daac0cec0abacea513b13809cce92
Madalina <madalina.zamfira@gmail.com>
2020-10-12 14:31:47 +0200

    added renderAction method to track component and transformed it in class

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
 import './Track.css';
 
-const Track = () => {
+class Track extends React.Component {
+
+    renderAction(){
+        if (this.props.isRemoval) {
+          return <button className="Track-action">-</button>;
+        } else {
+          return <button className="Track-action">+</button>;
+        }
+      };
+    
+render(){
     return (
         <div className="Track">
             <div className="Track-information">
                 <h3>Track name</h3>
                 <p>Track artist | Track album</p>
             </div>
-            <button className="Track-action">+</button>
+            {this.renderAction}
         </div>
     )
+}
 }
 
 export default Track;
