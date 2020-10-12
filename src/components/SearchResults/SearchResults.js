b6dd02f0e5465676a210c842bdb1fc4bd165cd98
Madalina <madalina.zamfira@gmail.com>
2020-10-12 22:23:11 +0200

    pass the search results from the SearchResults component to the TrackList component

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TrackList from "../TrackList/TrackList";
 import "./SearchResults.css";
 
 class SearchResults extends React.Component {
@@ -6,6 +7,7 @@ class SearchResults extends React.Component {
     return (
       <div className="SearchResults">
         <h2>Results</h2>
+        <TrackList tracks={this.props.searchResults}/>
       </div>
     );
   }
