94f06cd704f9f27e78c3d37689a985d84330a530
Madalina <madalina.zamfira@gmail.com>
2020-10-12 22:32:59 +0200

    render the track name, artist and album

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -6,7 +6,7 @@ class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        <Track />
+        {this.props.tracks.map(track => <Track track={track} key={track.id}/>)}
       </div>
     );
   }
